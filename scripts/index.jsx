e3be3d1c6a7b2063c5802dc59877b56a4c011760
toketas <nishizuka23@gmail.com>
2017-11-06 00:50:12 -0200

    Update webpack configuration.

--- scripts/index.jsx
+++ scripts/index.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Header from './components/header.jsx';
 import Footer from './components/footer.jsx';
+import View from './components/view.jsx';
 
 class Main extends React.Component {
   render() {
     return <div>
       <Header/>
+      <View/>
       <Footer/>
     </div>;
   }
--- webpack.config.js
+++ webpack.config.js
@@ -1,47 +1,120 @@
-var path = require('path'),
-    webpack = require('webpack'),
-    extractTextPlugin = require('extract-text-webpack-plugin');
+var webpack = require('webpack');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var htmlWebpackPlugin = require('html-webpack-plugin');
+var path = require('path');
+var extractHTML = new ExtractTextPlugin('index.html');
+var extractLess = new ExtractTextPlugin('[name].css');
 
-var cssExtractTextPlugin = new extractTextPlugin('dist/styles.css');
+var config = {
+  devtool: 'source-map',
 
-const config = {
-  entry: {
-    "script": "./scripts/index.jsx",
-    "style": "./styles/index.less",
-  },
-  output: {
-    path: __dirname,
-    filename: "dist/[name].js",
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['react', 'es2015']
-        },
-        include: path.join(__dirname, 'scripts')
-      },
-      {
-        test: /\.css?$/,
-        loader: cssExtractTextPlugin.extract({ fallback: 'style-loader', use: 'css-loader' })
-      },
-      {
-        test: /\.less?$/,
-        loader: cssExtractTextPlugin.extract({ fallback: 'style-loader', use: 'css-loader!less-loader' })
-      },
-    ]
-  },
-  plugins: [
-    cssExtractTextPlugin,
-  ],
   devServer: {
     inline: true,
-    contentBase: './dist',
     port: 8000,
+    historyApiFallback: true,
   },
-}
+
+  entry: {
+    'script': './scripts/index.jsx',
+    'styles': './styles/index.less',
+    'index': './index.html',
+  },
+
+  output: {
+    path: path.join(__dirname, 'dist'),
+    publicPath: '/',
+    filename: '[name].js',
+    sourceMapFilename: "[name].map.js",
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: extractHTML.extract({
+          use: ['html-loader'],
+        }),
+      },
+      {
+        test: /\.less$/,
+        use: extractLess.extract({
+          fallback: 'style-loader',
+          use: [{
+            loader: 'less-loader',
+            loader: 'css-loader',
+          }],
+        }),
+      },
+      {
+        test: /\.css$/,
+        use: extractLess.extract({
+          use: [{
+            loader: 'less-loader',
+            loader: 'css-loader',
+          }],
+          fallback: 'style-loader',
+        }),
+      },
+      {
+        test: /\.jsx$/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react'],
+        },
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)\//,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.json$/,
+        use: ['json-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg.*)$/,
+        use: [{
+          loader: 'file-loader',
+          query: {
+            useRelativePath: false,
+          },
+        }, 'img-loader'],
+      },
+      {
+        test: /\.(ttf.*|eot.*|woff.*|ogg|mp3)$/,
+        use: [{
+          loader: 'file-loader',
+          query: {
+            useRelativePath: false,
+          },
+        }],
+      },
+      {
+        test: /\.po$/,
+        use: [
+          'json-loader',
+          {
+            loader: 'po-loader',
+            query: {format: 'jed1.x'},
+          },
+        ],
+      },
+    ],
+  },
+
+  plugins: [
+    extractHTML,
+    extractLess,
+    //new webpack.DefinePlugin({
+    //  'process.env': {NODE_ENV: JSON.stringify(process.env.NODE_ENV)},
+    //  Environment: JSON.stringify(require('config')),
+    //}),
+  ],
+
+  resolve: {
+    modules: [__dirname, path.join(__dirname, 'node_modules'), path.join(__dirname, 'scripts')],
+    extensions: ['.js', '.jsx', '.json'],
+  },
+};
 
 module.exports = config;
